c27783a63e4470d5e7bb0dd23b68b771197f2761
Manish Giri <manishgiri562@gmail.com>
2021-09-27 08:01:09 +0530

    Prepared App for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,9 +9,7 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
     try {
         // 1) Get checkout session from API
-        const session = await axios(
-            `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
-        );
+        const session = await axios(`/api/v1/booking/checkout-session/${tourId}`);
         // console.log(session);
         // Create checkout form and carge credit card
         // await stripe.redirectToCheckout({
