7f4f7fc871484ba3ca30600ac19316b478ccd4fc
Manish Giri <manishgiri562@gmail.com>
2021-09-27 09:55:08 +0530

    random commit

--- server.js
+++ server.js
@@ -27,3 +27,10 @@ process.on('unhandledRejection', (err) => {
         process.exit(1);
     });
 });
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM RECEIVED. Shutting down app...!!');
+    server.close(() => {
+        console.log('Process terminated..!');
+    });
+});
